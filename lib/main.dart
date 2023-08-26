a3c4dc253411d7a53a362f40f817b282c9077259
Nasrullah <nasrullah01n@gmail.com>
2023-08-26 19:33:29 +0800

    Initialized flutter in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:water_where_ah/firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
